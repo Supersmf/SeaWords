b85439eead7c776576e3d1f20fc42fd4898f560a
Oleg Gaidykevich <smfsuper@gmail.com>
2024-07-26 11:54:40 +0300

    fix packages  issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SeaWords/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
